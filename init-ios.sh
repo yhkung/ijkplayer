9c2d607ea5b894b492fd222a737e86d9ede688c6
ragingfire35 <ragingfire35@163.com>
2017-11-20 21:41:46 +0800

    comment checkout

--- init-ios.sh
+++ init-ios.sh
@@ -60,9 +60,9 @@ function pull_common() {
 function pull_fork() {
     echo "== pull ffmpeg fork $1 =="
     sh $TOOLS/pull-repo-ref.sh $IJK_FFMPEG_FORK ios/ffmpeg-$1 ${IJK_FFMPEG_LOCAL_REPO}
-    cd ios/ffmpeg-$1
-    git checkout ${IJK_FFMPEG_COMMIT} -B ijkplayer
-    cd -
+    #cd ios/ffmpeg-$1
+    #git checkout ${IJK_FFMPEG_COMMIT} -B ijkplayer
+    #cd -
 }
 
 function pull_fork_all() {
